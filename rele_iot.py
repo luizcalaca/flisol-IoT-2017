240038799057ada82fff115837913443723016a7
Luiz Fernando Calaça Silva <luizcalaca@gmail.com>
2017-04-07 16:20:42 -0200

    adição do on_disconnect

--- rele_iot.py
+++ rele_iot.py
@@ -12,12 +12,20 @@ RELE = 12
 
 def configure():
     GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
     GPIO.setup(RELE, GPIO.OUT)
+    print("Ligou")
 
 
 def on_connect(client, userdata, flags, rc):
     print("Conectado ao Broker. Resultado: "+ str(rc))
     client.subscribe(TopicoSubscribe)
+    
+
+def on_disconnect(client, userdata, flags, rc):
+    print("Conectado ao Broker. Resultado: "+ str(rc))
+    if(rc != 0):
+        print("Desconectado")
     
 
 def on_message(client, userdata, msg):
@@ -30,10 +38,13 @@ def on_message(client, userdata, msg):
 
 def turnLight(mensagemRecebida):
     if (str(mensagemRecebida) == "1"):
-        print("entrou 2")
-        GPIO.output(RELE, GPIO.LOW)
-        #time.sleep(2)
-        #GPIO.output(RELE, GPIO.LOW)
+       print("entrou 2")
+       GPIO.output(RELE, GPIO.HIGH)
+    elif (str(mensagemRecebida) == "0"):
+       print("entrou 3")
+       GPIO.output(RELE, GPIO.LOW)
+    else:
+        print("Entrada inválida")
 
 
 try:
@@ -41,8 +52,8 @@ try:
         configure()
         client = mqtt.Client()
         client.on_connect = on_connect
+        client.on_disconnect = on_disconnect
         client.on_message = on_message
-
         client.connect(Broker, PortaBroker, KeepAliveBroker)
         client.loop_forever()
 except KeyboardInterrupt:
